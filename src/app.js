00f78b22d0fe1cdf54bfb0e34cde08e0a174327f
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2019-08-26 16:02:05 +0100

    its getting there somehow

--- src/app.js
+++ src/app.js
@@ -4,10 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
 	new Vue({
 		el: "#app",
 		data: {
-      
+      todos: [{name: "wash car", priority: "low"},
+            {name: "hoover", priority: "low"},
+            {name: "walk dog", priority: "high"}],
+
+      newTask: ""
 		},
 		methods: {
-
-			}
+      saveNewTask: function () {
+        if (this.newTask !== ""){
+          const taskToAdd = {name: this.newTask, priority: this.priority};
+          this.todos.push(taskToAdd);
+        };
+        this.newTask = "";
+      }
+		}
 		});
 });
